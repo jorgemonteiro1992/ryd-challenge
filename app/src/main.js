2db80f7ff9a1fc730dcd91d26a441f96ffc2e31b
Jorge Monteiro <jorge_adolfo18@hotmail.com>
2023-11-07 01:26:04 +0000

    Chore: Prime Vue installation
    Installations of library that allows us to create components easier.
    It will be used to create inputs

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import '@/styles/global.scss';
+import PrimeVue from "primevue/config";
+import 'primevue/resources/themes/lara-light-blue/theme.css';
 import App from './App.vue';
 
 createApp( App )
+    .use( PrimeVue )
     .mount( '#app' );
